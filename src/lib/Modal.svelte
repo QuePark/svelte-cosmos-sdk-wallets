4203dece6c7141646faceb2b1d5a298698b5deb1
QuePark <gyuhapark11@gmail.com>
2023-06-15 16:54:01 +0900

    🐛 Output message when props is exist

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,6 +1,6 @@
 <script>
 // @ts-nocheck
-  export let message = 'close';
+  export let message = '';
   export let showModal = true;
   export let isPromo = true;
   export let modalClassName = '';
@@ -12,11 +12,13 @@
   <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal {showModal ? 'modal-open': ''} {modalClassName === '' ? 'h-1/6' : modalClassName} h-fit md:min-w-fit md:w-full">
         <slot />
-        {#if message !== ''}
-        <button class="btn btn-circle" on:click={() => showModal = false}>
+        <button class="btn {message === '' ? 'btn-circle' : ''}" on:click={() => showModal = false}>
+        {#if message === ''}
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-        </button>
+        {:else}
+            {message}
         {/if}
+        </button>
     </div>
   </div>
 {/if}
