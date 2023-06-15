f0e2961875be4a6b653e8da58a084689c3dbacd5
QuePark <gyuhapark11@gmail.com>
2023-06-15 16:36:13 +0900

    💄 Remove unused props in home component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,4 @@
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
-<WalletConnectButton buttonClassName="btn-neutral" primary label="Connect" />
+<WalletConnectButton innerButtonClassName="btn-neutral" label="Connect" />
