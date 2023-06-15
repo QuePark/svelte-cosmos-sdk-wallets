c9c0f0038951a67a8c71429c889b252d794d785b
QuePark <gyuhapark11@gmail.com>
2023-06-15 17:05:53 +0900

    🎨 Apply prettier

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,16 +1,18 @@
-<script lang="ts">
-    export let func = () => {};
-    export let buttonClassName = '';
-
-    function onClick() {
-        func();
-    }
-</script>
-
-<button class="btn btn-custom my-4 mx-auto min-w-fit w-52 {buttonClassName}" on:click={onClick}><slot /></button>
-
 <style>
 .btn-custom {
-    text-transform: none;
+	text-transform: none;
 }
 </style>
+
+<script lang="ts">
+export let func = () => {};
+export let buttonClassName = '';
+
+function onClick() {
+	func();
+}
+</script>
+
+<button class="btn-custom btn mx-auto my-4 w-52 min-w-fit {buttonClassName}" on:click="{onClick}"
+	><slot /></button
+>
